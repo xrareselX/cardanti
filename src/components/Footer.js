7bad2487d70aa617316be75843cd5b4ecb80c22f
Rares Tamaian <rarres_da@yahoo.com>
2023-03-16 18:22:18 +0200

    navbar css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,40 @@ import { useTranslation } from "react-i18next";
 import Form from "react-bootstrap/Form";
 import ReactFlagsSelect from "react-flags-select";
 import {Us} from "react-flags-select";
+import Select from "react-select";
+import { useState } from "react";
 
 function Footer() {
 
+    const data = [
+        {
+            value: "en",
+            text: 'English',
+            icon: <img src="https://flagcdn.com/us.svg" width="20"/>
+          },
+          {
+            value: "ro",
+            text: 'Română',
+            icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
+            </svg>
+          },
+          {
+            value: "it",
+            text: 'Italiano',
+            icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+            </svg>
+          },
+          {
+            value: "de",
+            text: 'Right Arrow',
+            icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+            </svg>
+          }
+    ];
+    const {selectedOption, setSelectedOption} = useState(null);
     const { t, i18n } = useTranslation();
     //Creating a method to change the language onChange from select box
   const changeLanguageHandler = (e) => {
@@ -22,12 +53,23 @@ function Footer() {
                         <div className="align-items-center flex-grow-0 pl-2 select-language">
                              {/* Select box to change language */}
                             <Form.Select className="custom-select"  onChange={changeLanguageHandler}>
-                                <option value="en" > English</option>
+                                <option value="en" >English</option>
                                 <option value="ro" selected>Română</option>
                                 <option value="it" >Italiano</option>
                                 <option value="es" >Español</option>
                                 <option value="de" >Deutsche</option>
                             </Form.Select>
+                            {/* <Select value={selectedOption} options={data} className="custom-select" 
+                             onChange={changeLanguageHandler} defaultValue="ro"
+                             getOptionLabel={e => (
+                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                  {e.icon}
+                                  <span style={{ marginLeft: 5 }}>{e.text}</span>
+                                </div>
+                              )} 
+                                >
+                                
+                            </Select> */}
                         </div>
                     </div>
                     <div className="col-12 col-sm-8 mb-4 order-sm-3">
@@ -36,10 +78,10 @@ function Footer() {
                             <Link to='/privacy-policy'>Politica de confidențialitate</Link>
                             <Link to='/terms-and-condition'>Termeni și condiții</Link>
                             <Link to='/contact'>Contact</Link>
-                            <ReactFlagsSelect countries={["US", "RO", "IT", "ES", "DE"]} customLabels={{"DE":"UVYFCT"}}
-    selected={"ro"}
-    onSelect={changeLanguageHandler}
-  />;
+                            {/* <ReactFlagsSelect countries={["US", "RO", "IT", "ES", "DE"]} customLabels={{"DE":"UVYFCT"}}
+                                    selected={"ro"}
+                                    onSelect={changeLanguageHandler}
+                                />; */}
                         </div>
                     </div>
                 </div>
@@ -70,7 +112,11 @@ function Footer() {
                 <div className="row copyright">
                     <div className="col-12 text-center">
                         @ Cardanti 2020
-                        <Us />
+                        
+                        {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+      </svg>
+                        <Us /> */}
                     </div>
                 </div>
             </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,65 @@
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTiktok } from "@fortawesome/free-brands-svg-icons";
+import { useTranslation } from "react-i18next";
+import Button from "react-bootstrap/Button"
+import { useState } from "react";
+
+
 
 function Nav() {
+
+    const [isScrolled, setIsScroller] = useState(false);
+
+    const addScrolledClass = () => {
+        const scrollValue = document.documentElement.scrollTop;
+        if (scrollValue > 0) {
+            setIsScroller(true);
+        } else{
+            setIsScroller(false);
+        }
+    }
+    window.addEventListener("scroll", addScrolledClass);
+    const {t, i18n} = useTranslation();
+
     return (
-        <header className="navbar navbar-expand-lg navbar-header">
+        <header className={"sticky-top navbar navbar-expand-lg navbar-header " + (isScrolled ? "scrolled" : "")}>
+            <div className="container">
         {/* <nav className="navbar navbar-expand-lg navbar-header"> */}
-        <a class="navbar-brand" href="/"><FontAwesomeIcon icon={faTiktok} size=""/>Cardanti</a>
+        <div className="navbar-brand">
+        <a class="" href="/"><FontAwesomeIcon icon={faTiktok} size=""/>Cardanti</a>
+        </div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse navbar-right"  id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/about-us">Despre noi</Link>
-                        </li>  {/* nu merg astea pt ca nu sunt in contextul de routing ai pe stackoverflow detalii */}
-                        <li><Link className="nav-link" to='/new-meetup'>Business</Link></li>
-                        <li><Link className="nav-link" to='/favorites'>Cum funcționează
-                        <span></span>
-                        </Link></li>
-                        <li><Link className="nav-link" to='/new-meetup'>Întrebări frecvente</Link></li>
-                        <li><Link className="nav-link" to='/new-meetup'>Limba steag</Link></li>
-                        <li><Link className="nav-link" to='/new-meetup'>Login</Link></li>
-                        <li><Link className="nav-link" to='/buy-now'>Cumpără acum</Link></li>
-                </ul>
+            <div className="navbar-right collapse navbar-collapse"  id="navbarSupportedContent">
+                {/* <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active"><Link className="nav-link" to="/about-us">Despre noi</Link></li>
+                    <li><Link className="nav-link" to='/new-meetup'>Business</Link></li>
+                    <li><Link className="nav-link" to='/favorites'>Cum funcționează</Link></li>
+                    <li><Link className="nav-link" to='/new-meetup'>Întrebări frecvente</Link></li>
+                    <li><Link className="nav-link" to='/new-meetup'>Limba steag</Link></li>
+                    <li><Link className="nav-link" to='/new-meetup'>Login</Link></li>
+                    <li><Link className="nav-link" to='/buy-now'>Cumpără acum</Link></li>
+                </ul> */}
+                <div className="navbar-nav navbar-right"> 
+                    <div className="top-links navbar-nav"> {/** navbar-nav ofera flex-direction: row */}
+                        <Link className="top-link" to="/about-us">{t('nav.aboutUs')}</Link>
+                        <Link className="top-link" to='/new-meetup'>Business</Link>
+                        <Link className="top-link" to='/favorites'>{t('nav.how-it-works')}</Link>
+                        <Link className="top-link" to='/new-meetup'>{t('nav.questions')}</Link>
+                    </div>
+                        <Link className="" to='/new-meetup'>Limba steag selector</Link>
+                        <Link className="btn-login" to='/login'>{t('nav.login')}</Link>
+                        <Link to="buy-now">
+                            <Button className="btn-green btn-small btn-uppercase">
+                                {t("nav.buy-now")}
+                            </Button>
+                        </Link>
+                </div>
             </div>
         {/* </nav> */}
+        </div>
     </header>
     );
 }
