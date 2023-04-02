066147a71204d9a50ec25757fc66ab8ee0dccd76
Rares Tamaian <rarres_da@yahoo.com>
2023-04-02 20:56:32 +0300

    navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -30,47 +30,58 @@ function Navigation() {
     const {t, i18n} = useTranslation();
 
     return (
-        <header className={"sticky-top navbar navbar-expand-lg navbar-header " + (isScrolled ? "scrolled" : "")}>
-            <Navbar collapseOnSelect expand="lg">
-                <Navbar.Brand href="/">Brand Name</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link href="#link1">Link 1</Nav.Link>
-                        <Nav.Link href="#link2">Link 2</Nav.Link>
-                        <Nav.Link href="#link3">Link 3</Nav.Link>
-                        <Nav.Link href="#link4">Link 4</Nav.Link>
-                     </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        {/* <div className="container">
-        <div className="navbar-brand">
-        <a className="" href="/"><CardantiC width="30"/><CardantiName width="90" className="logo-long"/></a>
-        </div>
-            <button className="navbar-toggler" type="button" dataToggle="collapse" dataTarget="#navbarSupportedContent" ariaControls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="navbar-right collapse navbar-collapse"  id="navbarSupportedContent">
-                <div className="navbar-nav navbar-right"> 
-                    <div className="top-links navbar-nav">
-                        <Link className="top-link" to="/about-us">{t('nav.aboutUs')}</Link>
-                        <Link className="top-link" to='/business'>Business</Link>
-                        <Link className="top-link" to='/how_it_works'>{t('nav.how-it-works')}</Link>
-                        <Link className="top-link" to='https://help.cardanti.com/help' target="_blank">{t('nav.questions')}</Link>
-                    </div>
+        <header className="navbar navbar-header navbar-header-fixed">
+            <div className="container">
+                <div className="navbar-brand">
+                    <a href="/" className="logo">
+                    cele 2 svg 
+                     </a>
+                </div>
+                <div className="navbar-right">
+                    <div className="top-links">
+                        <a href="/about-us" className="top-link">{t('nav.aboutUs')}</a>
+                        <a href="/business" className="top-link">Business</a> 
+                        <a href="/how_it_works" className="top-link">{t('nav.how-it-works')}</a> 
+                        <a href="https://help.cardanti.com/" target="_blank" className="top-link">{t('nav.questions')}</a>
+                    </div> 
+                    <div className="locale-selector">
                         <LanguageSwitcher />
-                        <Link className="btn-login" to='/login'>{t('nav.login')}</Link>
-                        {location.pathname != "/order-card" && (
-                            <Link to="order-card">
-                                <Button className="btn-green btn-small btn-uppercase">
-                                    {t("nav.buy-now")}
-                                </Button>
-                            </Link>
-                        )}
+                    </div>
+                    <a className="btn btn-sm btn-login">{t('nav.login')}</a>
+                    <button className="btn btn-sm btn-green btn-uppercase btn-order-card">{t("nav.buy-now")}</button>
                 </div>
             </div>
-        </div> */}
-    </header>
+        </header>
+        //  <header className={"sticky-top navbar navbar-expand-lg navbar-header " + (isScrolled ? "scrolled" : "")}>
+
+        //  <div className="container">
+        // <div className="navbar-brand">
+        // <a className="" href="/"><CardantiC width="30"/><CardantiName width="90" className="logo-long"/></a>
+        // </div>
+        //     <button className="navbar-toggler" type="button" dataToggle="collapse" dataTarget="#navbarSupportedContent" ariaControls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        //         <span className="navbar-toggler-icon"></span>
+        //     </button>
+        //     <div className="navbar-right collapse navbar-collapse"  id="navbarSupportedContent">
+        //         <div className="navbar-nav navbar-right"> 
+        //             <div className="top-links navbar-nav">
+        //                 <Link className="top-link" to="/about-us">{t('nav.aboutUs')}</Link>
+        //                 <Link className="top-link" to='/business'>Business</Link>
+        //                 <Link className="top-link" to='/how_it_works'>{t('nav.how-it-works')}</Link>
+        //                 <Link className="top-link" to='https://help.cardanti.com/help' target="_blank">{t('nav.questions')}</Link>
+        //             </div>
+        //                 <LanguageSwitcher />
+        //                 <Link className="btn-login" to='/login'>{t('nav.login')}</Link>
+        //                 {location.pathname != "/order-card" && (
+        //                     <Link to="order-card">
+        //                         <Button className="btn-green btn-small btn-uppercase">
+        //                             {t("nav.buy-now")}
+        //                         </Button>
+        //                     </Link>
+        //                 )}
+        //         </div>
+        //     </div>
+        // </div> 
+    //  </header> 
     );
 }
 
