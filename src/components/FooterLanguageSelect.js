aa35c0a2c84b9fec61c357d23814fbde8bbc89b0
Rares Tamaian <rarres_da@yahoo.com>
2023-04-07 00:07:21 +0300

    on clik outside close dropdown functionality

--- src/components/FooterLanguageSelect.js
+++ src/components/FooterLanguageSelect.js
@@ -2,31 +2,40 @@ import roImg from "../countries/footer_ro.svg";
 import usImg from "../countries/footer_en.svg";
 import deImg from "../countries/footer_de.svg";
 import Message from "../assets/icons/Message";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import DownArrow from "../assets/icons/DownArrow";
 import { useTranslation } from "react-i18next";
 function FooterLanguageSelect(props) {
     const {t,  i18n } = useTranslation();
     const [isOpen, setIsOpen] = useState(false);
     const [selectedCountry, setSelectedCountry] = useState("Română"); 
+    const menuRef = useRef(null);
+    const selectRef = useRef(null);
     const options = [
         { value: 'ro', label: 'Română', flag: roImg },
         { value: 'en', label: 'English', flag: usImg },
         { value: 'de', label: 'Deutsch', flag: deImg },
       ];
+
       const handleOptionClick = (value, label) => {
         setSelectedCountry(label);
         setIsOpen(false);
         i18n.changeLanguage(value);
       };
+    const closeOpenMenu = (e)=>{
+        if(menuRef.current && isOpen && !menuRef.current.contains(e.target) && !selectRef.current.contains(e.target)){
+            setIsOpen(false)
+        }
+    }
+    document.addEventListener('mousedown', closeOpenMenu)
   return (
     <div className="custom-select">
-    <span className="selected-value" onClick={() => setIsOpen(!isOpen)}>
+    <span ref={selectRef} className="selected-value" onClick={() => setIsOpen(!isOpen)}>
       <Message className="svg-message" />
       {selectedCountry}
       <DownArrow className={`svg-arrow ` +  (isOpen ? "rotate-svg" :"")}/>
     </span>
-    <div className={`options-container ${isOpen ? 'active' : ''}`}>
+    <div ref={menuRef} className={`options-container ${isOpen ? 'active' : ''}`}>
       <ul className="options-list">
         {options.map((option) => (
           <li
