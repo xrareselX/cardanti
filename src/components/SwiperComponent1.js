80877ac38c80c809a0f871320e755bc88d17f101
Rares Tamaian <rarres_da@yahoo.com>
2023-04-13 21:45:53 +0300

    clean up code in swiper component

--- src/components/SwiperComponent1.js
+++ src/components/SwiperComponent1.js
@@ -23,16 +23,16 @@ function SwiperComponent1(props){
             slidesPerView={"auto"}
             // slidesPerView={1}
             onSlideChangeTransitionStart={props.displayCardTitle}
-            // breakpoints={{
-            //     100: {
+            breakpoints={{
+                100: {
             //         width: 250,
-            //         slidesPerView: 1
-            //     },
-            //     1200:{
+                    slidesPerView: 1
+                },
+                1200:{
             //         width: 400,
-            //         slidesPerView: "auto"
-            //     }
-            // }}
+                    slidesPerView: "auto"
+                }
+            }}
         >
             {/* {props.displayFlipModal && (
                 <div className="flip-it-overlay overlay-tooltip mb-3" onClick={props.flipModalClickedHandle}>
@@ -52,20 +52,10 @@ function SwiperComponent1(props){
                     > */}
                     <div data-id="2015" className="carousel-slide-container container-padding customizer-card d-flex justify-content-center align-items-center">
                         <div className="customizer-container d-flex justify-content-center align-items-center">
-                            {/* <img
-                             style={{ width: "100%", zIndex: "-1"}}
-                             src={card["img-source"]} alt="" /> */}
-                            {/* <img
-                             style={{ width: "100%"}}
-                             src={card["img-back"]} alt="" /> */}
                             <div className="image-wrapper align-self-center">
                                 {props.displayFlipModal && (
-                                    <div className={`flip-it-overlay overlay-tooltip mb-3 ${props.overlayClass}`} onClick={props.flipModalClickedHandle}
-                                    // style={{height: "260px"}}
-                                    >
-                                        <div className="w-100 d-flex justify-content-center">
-                                            {/* <flip-card></flip-card> */}
-                                        </div>
+                                    <div className={`flip-it-overlay overlay-tooltip mb-3 ${props.overlayClass}`} onClick={props.flipModalClickedHandle}>
+                                        <div className="w-100 d-flex justify-content-center"></div>
                                         <p className="text-white">{t("order_card.flip_modal")}</p>
                                     </div>
                                     )}
@@ -95,7 +85,6 @@ function SwiperComponent1(props){
                                             <img
                                                 style={{ width: "100%"}}
                                                 src={card["img-source"]} alt="" />
-                                            {/* <img src="https://cardanti.com/_nuxt/img/tech-min.61b519b.webp" className="card-img-main" /> */}
                                             <div className="custom-data">
                                                 <div className="name-data card-inner-text name" style={{color: "white", fontSize: `${props.nameSize}pt`}}>
                                                     {props.nameText}
@@ -116,46 +105,6 @@ function SwiperComponent1(props){
                             </div>
                         </div>
                     </div>
-                        {/* aici sub e ala vechi */}
-                    {/* <div className="card__face card__face--front">
-                        <div className="card-logo" >
-                            {props.cardantiLogo && !props.emptyLogo && (
-                                <div className="card-logo-svg">
-                                    <CardantiC/>    
-                                </div>
-                                )}
-                            {!props.cardantiLogo && !props.emptyLogo && (
-                                <div className="card-logo-svg text-white" >
-                                    <div className="card-logo">
-                                        <div className="card-logo-placeholder" style={{borderColor: "rgba(255, 255, 255, 0.6)"}}>
-                                            <span style={{color: "rgba(255, 255, 255, 0.6)"}}>
-                                                Logo-ul tău
-                                            </span>
-                                        </div>
-                                        <div className="preview w-100 d-flex justify-content-center" style={{overflow: "hidden"}}>
-                                            <canvas width="200" height="80" style={{visibility: "visible"}}>
-                                            </canvas>
-                                        </div>
-                                    </div>  
-                                </div>
-                                )}
-                        </div>
-                        <img style={{ width: "100%"}} src={card["img-source"]} alt="" />
-                        <div className="custom-data">
-                            <div  className="name-data card-inner-text name" style={{color: "white", fontSize: `${props.nameSize}pt`}}>
-                            </div>
-                            <div  className="role-data card-inner-text role" style={{color: "white", fontSize: `${props.roleSize}pt`}}>
-                            </div>
-                            <div  className="company-data card-inner-text company" style={{color: "white", fontSize: `${props.companySize}pt`}}>
-                            </div>
-                        </div>
-                    </div> */}
-                    {/* <div className="card__face card__face--back text-white">
-                        <img style={{ width: "100%"}} 
-                        src={card["img-back"]} alt="" />
-                    </div> */}
-                    {/* below is the wrapper div */}
-                    {/* </div> */}
                 </SwiperSlide>
             ))
             }
